0c29508897ae5373bf9a88d077f43ea1519c5d06
Martipetti <78962893+Martipetti@users.noreply.github.com>
2021-12-27 22:26:50 +0100

    Create parse.lisp

---
+++ parse.lisp
@@ -0,0 +1,28 @@
+(defun uri-parse (stringa)
+  (let ((lista (coerce stringa 'list)))
+  (if (null stringa) (error "stringa vuota")
+    (set-scheme lista))))
+        
+(defun set-scheme (lista)
+  (let ((scheme (list-id lista #\:))
+        (rest (id-list lista #\:)))
+    (if (null scheme) (error "URI non valida")
+      (coerce scheme 'string))))
+
+;ritorna la lista da un id in po
+(defun id-list (lista id)
+ (if (null lista) nil
+   (if (equal (car lista) id) (cdr lista)
+     (id-list (cdr lista) id))))
+;ritorna la lista dall'inizio fino ad un certo id
+(defun list-id (lista id)
+  (if (null lista) '()
+    (list-to-id (rev lista) id)))
+(defun list-to-id (lista id)
+  (if (null lista) nil
+    (if (equal (car lista) id) (rev (cdr lista))
+      (list-to-id (cdr lista) id))))
+;funzione di reverse
+(defun rev (l) 
+  (cond ((null l) '())
+        (t (append (rev (cdr l)) (list (car l)))))) 
