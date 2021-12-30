b468ccf76d0d7290a62ef8466ffac9a5c36334aa
DaniMerlo <95621615+DaniMerlo@users.noreply.github.com>
2021-12-30 11:46:48 +0100

    Update parse.lisp

--- parse.lisp
+++ parse.lisp
@@ -4,13 +4,22 @@
   (let ((lista (coerce stringa 'list)))
   (if (null stringa) (error "stringa vuota")
     (and (set-scheme lista)
-    (make-uri :scheme scheme-def)))))
-        
+    (make-uri :scheme scheme-def
+              :host host-1
+              :path path-1     ;campi aggiuntivi uri
+              :query query-1
+              :fragment fragment-1)))))
+;metodo di gestione dello scheme (controllare)       
 (defun set-scheme (lista)
   (let ((scheme (list-id lista #\:))
         (rest (id-list lista #\:)))
     (if (not (string-id scheme)) (error "URI non valida")
-      (setq scheme-def (coerce scheme 'string)))))
+      (and (setq scheme-def (coerce scheme 'string))
+        (if(and (eq (car rest) #\/)
+                (eq (car (cdr rest)) #\/))
+                (authority rest)
+                (coda rest)       
+                       )))))
 
 ;ritorna la lista da un id in poi
 (defun id-list (lista id)
@@ -41,8 +50,41 @@
          (eq (car scheme) #\@)
          (eq (car scheme) #\:)) nil 
          (cdr scheme)
-         ))     
+         ))    
+;controllo query         
 (defun query-id (query)
   (if (eq (car scheme) #\#) nil 
          (cdr scheme)
          ))    
+;authority (solo prova)
+(defun authority (aut)
+(if (null aut) (error "aut non valida")
+      (setq host-1 (coerce aut 'string))))
+
+;metodo per gestire la query (probabilmente da rivedere)
+(defun query (q)
+(if (eq (car q) #\?) 
+      (let ((query1 (list-id lista #\?))
+            (r (id-list lista #\?)))
+           (if (not (query-id path)) (error "URI non valida")
+           (and (setq query-1 (coerce query1 'string))
+             (if (eq (car r) #\#)
+                (fragment r)    
+                       ))))))
+;metodo per gestire il fragment (forse da rivedere)
+(defun fragment (f)
+(if (eq (car f) #\#) 
+      (let ((fragment1 (list-id lista #\#))
+       (setq fragment-1 (coerce fragment1 'string))))))
+                                    
+;metodo per gestire path,query e fragment semplificato 
+(defun coda (rest)
+(if (eq (car rest) #\/)   ;al posto di if si potrebbe usare anche when 
+      (let ((path (list-id lista #\/))
+            (r (id-list lista #\/)))
+           (if (not (identificatore-id path)) (error "URI non valida")
+           (and (setq path-1 (coerce path 'string))
+             (if (eq (car r) #\?)
+                (query r)    
+                       ))))))
+      
